e35deb94b597c527d68ca4b6e8cb720d11daee9b
Valerchik <new-house-drive@protonmail.com>
2023-10-10 22:15:52 +0300

    add onClick event

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Essentially we are telling the React.. what I want to render?
+// whatever is in my render() method
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: "Valera",
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload. {this.state.name}{" "}
+            is gay!
+          </p>
+          <button 
+          onClick={() => {
+            
+          }}
+          >Become a gay!</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
