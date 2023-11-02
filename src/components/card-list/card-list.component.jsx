06d20c049bab2031688116be93642d15c6829d01
Valerchik <new-house-drive@protonmail.com>
2023-11-02 22:06:05 +0200

    Render a list of monster names

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,10 +1,20 @@
 import { Component } from "react";
 
 class CardList extends Component {
-    render(){
-        console.log(this.props)
-        return <div>Hello, I am Valera kardless komponent</div>
-    }
+  render() {
+    console.log(this.props);
+    const {monsters} = this.props
+    return (
+        monsters.map((monster) => {
+            return (
+            <div key={monster.id}>
+                <h3>{monster.name}</h3>
+            </div>
+            )
+        })
+      
+    );
+  }
 }
 
 export default CardList;
