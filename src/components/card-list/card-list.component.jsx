1651b3354368d8dce010bf93792f5d95fe230668
Valerchik <new-house-drive@protonmail.com>
2023-11-10 02:35:33 +0200

    add card-container

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -7,9 +7,16 @@ class CardList extends Component {
 
     return (
       <div>
-        {monsters.map((monster) => (
-          <h3 key={monster.id}>{monster.name}</h3>
-        ))}
+        {monsters.map((monster) => {
+          // <h3 key={monster.id}>{monster.name}</h3>
+          const { name, email, id} = monster
+          return (
+          <div className="card-container" key={id}>
+            <img alt={`monster ${name}`} src={`https://robohash.org/${id}?set=set1&size=180x180`} />
+            <h2>{name}</h2>
+            <p>{email}</p>
+          </div>)
+        })}
       </div>
     );
   }
