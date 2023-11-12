0b3e9aa24f0b546b81d637ae4f7bad40f9da0bde
Valerchik <new-house-drive@protonmail.com>
2023-11-12 04:14:59 +0200

    extract card-component from card-list

---
+++ src/components/card-container/card-container.component.jsx
@@ -0,0 +1,19 @@
+import { Component } from "react";
+import "./card-container.styles.css"
+
+class CardContainer extends Component {
+    render() {
+
+        const {name, id, email } = this.props
+
+        return (
+            <div className="card-container" key={id}>
+            <img alt={`monster ${name}`} src={`https://robohash.org/${id}?set=set1&size=180x180`} />
+            <h2>{name}</h2>
+            <p>{email}</p>
+          </div>
+          )
+    }
+}
+
+export default CardContainer;
