cf08615f681a9efcbcfa792a4b5862cf41e21ad9
Valerchik <new-house-drive@protonmail.com>
2023-11-18 01:37:51 +0200

    update card-container to functional component

--- src/components/card-container/card-container.component.jsx
+++ src/components/card-container/card-container.component.jsx
@@ -1,22 +1,25 @@
 import { Component } from "react";
 import "./card-container.styles.css";
 
-const CardContainer = ({ name, id, email }) => {
-  <div className="card-container" key={id}>
-    <img
-      alt={`monster ${name}`}
-      src={`https://robohash.org/${id}?set=set1&size=180x180`}
-    />
-    <h3>{name}</h3>
-    <p>{email}</p>
-  </div>;
+const CardContainer = ({ monster }) => {
+  const { id, name, email } = monster;
+  return (
+    <div className="card-container" key={id}>
+      <img
+        alt={`monster ${name}`}
+        src={`https://robohash.org/${id}?set=set1&size=180x180`}
+      />
+      <h3>{name}</h3>
+      <p>{email}</p>
+    </div>
+  );
 };
 
 // Old CardContainer Class component
 // class CardContainer extends Component {
 //     render() {
 
-//         const {name, id, email } = this.props
+//         const {name, id, email } = this.props.monster
 
 //         return (
 //             <div className="card-container" key={id}>
