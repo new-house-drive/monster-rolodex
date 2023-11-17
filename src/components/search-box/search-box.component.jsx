dd327e8757fd16c57d23627a416857b760656318
Valerchik <new-house-drive@protonmail.com>
2023-11-18 01:41:25 +0200

    update searchBox to functional component

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,28 +1,40 @@
 import { Component } from "react";
 import './search-box.styles.css'
 
-class SearchBox extends Component {
-
-    render() {
-        return (
-            <input
-            // those are props!
+const SearchBox = (props) => {
+    const {className, placeholder, onChangeHandler} = props
+    return (
+        <input
             type="search"
-            className={`search-box ${this.props.className}`}
-            placeholder={this.props.placeholder}
-            /**
-             * The `onChange` method is a callback function used to handle changes in form input
-             * elements like text fields, checkboxes, and radio buttons. It is triggered when the
-             * user interacts with the input, such as typing or making a selection.
-             *
-             * This method is typically used to update the component's state or perform specific
-             * actions in response to user input, making it a crucial part of form handling in React.
-             */
-  
-            onChange={this.props.onChangeHandler}
+            className={`search-box ${className}`}
+            placeholder={placeholder}
+            onChange={onChangeHandler}
           />
-        )
-    }
+    )
 }
+
+// class SearchBox1 extends Component {
+
+//     render() {
+//         return (
+//             <input
+//             // those are props!
+//             type="search"
+//             className={`search-box ${this.props.className}`}
+//             placeholder={this.props.placeholder}
+//             /**
+//              * The `onChange` method is a callback function used to handle changes in form input
+//              * elements like text fields, checkboxes, and radio buttons. It is triggered when the
+//              * user interacts with the input, such as typing or making a selection.
+//              *
+//              * This method is typically used to update the component's state or perform specific
+//              * actions in response to user input, making it a crucial part of form handling in React.
+//              */
+  
+//             onChange={this.props.onChangeHandler}
+//           />
+//         )
+//     }
+// }
 
 export default SearchBox
