3903ef363f1dc5e503bea40453031a71d9986760
Valerchik <new-house-drive@protonmail.com>
2023-11-18 01:34:53 +0200

    update CardList to functional component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,21 +1,32 @@
 import { Component } from "react";
-import "./card-list.styles.css"
-import CardContainer from '../card-container/card-container.component.jsx'
+import "./card-list.styles.css";
+import CardContainer from "../card-container/card-container.component.jsx";
 
-class CardList extends Component {
-  render() {
-    console.log(this.props.monsters);
-    const { monsters } = this.props;
+const CardList = (props) => {
+  return (
+  <div className="card-list">
+    {props.monsters.map((monster) => {
+      return <CardContainer monster={monster} key={monster.id} />;
+    })}
+  </div>)
+};
 
-    return (
-      <div className="card-list">
-        {monsters.map((monster) => {
-          const { name, email, id } = monster;
-          return <CardContainer name={name} email={email} id={id} key={id} />;
-        })}
-      </div>
-    );
-  }
-}
+// Deprecated Class component
+
+// class CardList extends Component {
+//   render() {
+//     console.log(this.props.monsters);
+//     const { monsters } = this.props;
+
+//     return (
+//       <div className="card-list">
+//         {monsters.map((monster) => {
+//           const { name, email, id } = monster;
+//           return <CardContainer name={name} email={email} id={id} key={id} />;
+//         })}
+//       </div>
+//     );
+//   }
+// }
 
 export default CardList;
